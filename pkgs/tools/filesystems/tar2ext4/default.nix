2cc22719154ed8c6548608514a9e5a5a9b588ce3
Alyssa Ross <hi@alyssa.is>
2021-08-19 12:22:06 +0000

    tar2ext4: 0.8.22 -> unstable-2021-10-20

--- pkgs/tools/filesystems/tar2ext4/default.nix
+++ pkgs/tools/filesystems/tar2ext4/default.nix
@@ -1,14 +1,14 @@
 { lib, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
-  pname = "tar2ext4";
-  version = "0.9.2";
+  pname = "tar2ext4-unstable";
+  version = "2021-10-20";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "hcsshim";
-    rev = "v${version}";
-    sha256 = "sha256-Qa93MpTAMCA7vn7LefILDdAKYyRDPPq0vBwmIfegyuU=";
+    rev = "60b5fa7eea6f95295888d71b0621eb1c1291fb67";
+    sha256 = "0g5bj99786pwms7zklh3i8hvxg3dqsb5nrbqf8aifr3kp2cn9njh";
   };
 
   sourceRoot = "source/cmd/tar2ext4";
