816b98fc985729ea509ea0253c977450ec6983e2
Ville Ilvonen <ville.ilvonen@unikie.com>
2022-06-23 14:06:18 +0300

    aarch64: include imx8 to kernel configurations
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -814,6 +814,7 @@ mapAliases ({
   linux_rpi2 = linuxKernel.kernels.linux_rpi2;
   linux_rpi3 = linuxKernel.kernels.linux_rpi3;
   linux_rpi4 = linuxKernel.kernels.linux_rpi4;
+  linux_imx8 = linuxKernel.kernels.linux_imx8;
 
   # Added 2020-04-04
   linuxPackages_testing_hardened = throw "linuxPackages_testing_hardened has been removed, please use linuxPackages_latest_hardened";
