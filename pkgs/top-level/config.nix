8d3f30d71f1f9243b7d03881bd39a6001e959b07
Marko Lindqvist <marko.lindqvist@unikie.com>
2022-09-08 22:41:05 +0300

    DO NOT MERGE HACK: Allow Unfree by default
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- pkgs/top-level/config.nix
+++ pkgs/top-level/config.nix
@@ -79,7 +79,7 @@ let
 
     allowUnfree = mkOption {
       type = types.bool;
-      default = false;
+      default = true;
       # getEnv part is in check-meta.nix
       defaultText = literalExpression ''false || builtins.getEnv "NIXPKGS_ALLOW_UNFREE" == "1"'';
       description = ''
