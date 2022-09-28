ce8c68ce52492c3c5e9e9c2462d616860b376100
Alyssa Ross <hi@alyssa.is>
2022-09-28 17:01:28 +0000

    wayland-proxy-virtwl: unstable-2021-12-05 -> unstable-2022-08-06
    Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>

--- pkgs/tools/wayland/wayland-proxy-virtwl/default.nix
+++ pkgs/tools/wayland/wayland-proxy-virtwl/default.nix
@@ -1,17 +1,19 @@
 { lib
 , fetchFromGitHub
 , ocamlPackages
+, pkg-config
+, libdrm
 }:
 
 ocamlPackages.buildDunePackage rec {
   pname = "wayland-proxy-virtwl";
-  version = "unstable-2021-12-05";
+  version = "unstable-2022-08-06";
 
   src = fetchFromGitHub {
     owner = "talex5";
     repo = pname;
-    rev = "d7f58d405514dd031f2f12e402c8c6a58e62a885";
-    sha256 = "0riwaqdlrx2gzkrb02v4zdl4ivpmz9g5w87lj3bhqs0l3s6c249s";
+    rev = "fa566c85ce7f75e98d66b9fe617dcc4d565cd48a";
+    sha256 = "sha256-rU3uXV/Y98248d922JTz+F5MtOALbljW2Xl4FBXhRH4=";
   };
 
   postPatch = ''
@@ -25,15 +27,17 @@ ocamlPackages.buildDunePackage rec {
   strictDeps = true;
   nativeBuildInputs = [
     ocamlPackages.ppx_cstruct
+    pkg-config
   ];
 
-  buildInputs = with ocamlPackages; [
+  buildInputs = [ libdrm ] ++ (with ocamlPackages; [
+    dune-configurator
     wayland
     cmdliner
     logs
     cstruct-lwt
     ppx_cstruct
-  ];
+  ]);
 
   doCheck = true;
 
