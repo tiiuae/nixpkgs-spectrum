ea0d944dbfbd9d5fb638002ae8c486b6a67b698e
Alyssa Ross <hi@alyssa.is>
2022-09-28 17:01:27 +0000

    ocamlPackages.wayland: 1.0 -> unstable-2022-05-07
    Required by the latest version of wayland-proxy-virtwl.
    
    Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>

--- pkgs/development/ocaml-modules/wayland/default.nix
+++ pkgs/development/ocaml-modules/wayland/default.nix
@@ -1,6 +1,6 @@
 { lib
 , buildDunePackage
-, fetchurl
+, fetchFromGitHub
 , xmlm
 , lwt
 , logs
@@ -12,15 +12,17 @@
 
 buildDunePackage rec {
   pname = "wayland";
-  version = "1.0";
+  version = "unstable-2022-05-07";
 
   minimumOCamlVersion = "4.08";
 
   useDune2 = true;
 
-  src = fetchurl {
-    url = "https://github.com/talex5/ocaml-wayland/releases/download/v${version}/wayland-${version}.tbz";
-    sha256 = "bf8fd0057242d11f1c265c11cfa5de3c517ec0ad5994eae45e1efe3aac034510";
+  src = fetchFromGitHub {
+    owner = "talex5";
+    repo = "ocaml-wayland";
+    rev = "1513420d35f3edb6ad2d9e1db0227e3cd9b9b76c";
+    sha256 = "sha256-UAomnvpIuf3V8vtxvGgxuTYoxsDxFDa4UXMbXYDkSIE=";
   };
 
   propagatedBuildInputs = [
