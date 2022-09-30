f8e1c16484dce2045de019e90d47d9bff8bcc848
Alyssa Ross <alyssa.ross@unikie.com>
2022-09-30 21:09:04 +0000

    cloud-hypervisor: add virtio-gpu support
    The virtio-bindings changes update the bindings for recent kernels,
    and the vhost change is cherry-picked from crosvm's fork of the crate
    to add support for their custom extensions.
    
    Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>

--- pkgs/applications/virtualization/cloud-hypervisor/default.nix
+++ pkgs/applications/virtualization/cloud-hypervisor/default.nix
@@ -1,4 +1,6 @@
-{ lib, stdenv, fetchFromGitHub, rustPlatform, pkg-config, dtc, openssl }:
+{ lib, stdenv, fetchCrate, fetchFromGitHub, fetchpatch, rustPlatform
+, pkg-config, dtc, openssl
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "cloud-hypervisor";
@@ -11,10 +13,68 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-choTT20TVp42nN/vS6xCDA7Mbf1ZuAE1tFQZn49g9ak=";
   };
 
+  vhost = fetchCrate {
+    pname = "vhost";
+    version = "0.4.0";
+    sha256 = "sha256-72tvycgdTI3pK1EpGoBEI0WajX7SYZvTI0lbU8ybdsc=";
+  };
+
+  vm-virtio = fetchFromGitHub {
+    owner = "rust-vmm";
+    repo = "vm-virtio";
+    rev = "7e203db6ed044217acd80dec5967c15d979491f8";
+    sha256 = "sha256-QLukQCPeWpletbKqhIruvevLMEepnQ4fKWVH42H7QM8=";
+  };
+
+  postUnpack = ''
+    unpackFile ${vhost}
+    mv vhost-* vhost
+
+    unpackFile ${vm-virtio}/crates/virtio-bindings
+
+    chmod -R +w vhost virtio-bindings
+  '';
+
+  cargoPatches = [
+    ./0001-build-use-local-vhost.patch
+    ./0002-build-use-local-virtio-bindings.patch
+    ./0003-virtio-devices-add-a-GPU-device.patch
+  ];
+
+  vhostPatches = [
+    ./vhost-vhost_user-add-shared-memory-region-support.patch
+  ];
+
+  virtioBindingsPatches = [
+    # https://github.com/rust-vmm/vm-virtio/pull/194
+    ./0001-virtio-bindings-regenerate-with-bindgen-0.60.1.patch
+    ./0002-virtio-bindings-remove-workaround-for-old-bindgen.patch
+    ./0003-virtio-bindings-regenerate-with-Glibc-2.36.patch
+    ./0004-virtio-bindings-regenerate-with-Linux-5.19.patch
+
+    ./0005-virtio-bindings-add-virtio-gpu-bindings.patch
+  ];
+
+  postPatch = ''
+    pushd ../vhost
+    for patch in $vhostPatches; do
+        echo applying patch $patch
+        patch -p1 < $patch
+    done
+    popd
+
+    pushd ../virtio-bindings
+    for patch in $virtioBindingsPatches; do
+        echo applying patch $patch
+        patch -p3 < $patch
+    done
+    popd
+  '';
+
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ] ++ lib.optional stdenv.isAarch64 dtc;
 
-  cargoSha256 = "sha256-mmyaT24he33wLI3zLOOKhVtzrPRyWzKgXUvc37suy5E=";
+  cargoSha256 = "164vr9xxrlpllac5db69ggasczq5vqb9zv60dkwbqabn8qm5cr1x";
 
   OPENSSL_NO_VENDOR = true;
 
