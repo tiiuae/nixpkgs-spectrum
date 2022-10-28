a885de0e8367adf9c2a1ade7b9eff1a73ea37ea6
Yuri Nesterov <yuri.nesterov@gmail.com>
2022-10-28 09:21:48 +0000

    cloud-hypervisor: fix crash on aarch64
    When running on aarch64 system cloud-hypervisor tries to call the
    set_shm_regions function in the gpu class and it results in crash
    because this function is not implemented. With that change we are
    returning a not supported error instead of panicking so that
    cloud-hypervisor is able to boot the guest virtual machine. This
    is a temporary solution. In the future we will need to add support
    for memory regions in the gpu device.
    
    Signed-off-by: Yuri Nesterov <yuriy.nesterov@unikie.com>

--- pkgs/applications/virtualization/cloud-hypervisor/default.nix
+++ pkgs/applications/virtualization/cloud-hypervisor/default.nix
@@ -39,6 +39,7 @@ rustPlatform.buildRustPackage rec {
     ./0001-build-use-local-vhost.patch
     ./0002-build-use-local-virtio-bindings.patch
     ./0003-virtio-devices-add-a-GPU-device.patch
+    ./0004-cloud-hypervisor-fix-crash-on-aarch64.patch
   ];
 
   vhostPatches = [
