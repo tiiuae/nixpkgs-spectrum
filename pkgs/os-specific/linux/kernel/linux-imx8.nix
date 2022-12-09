f13156c808e13da331389ef1cb0144cae0e7d360
Mika Tammi <mika.tammi@unikie.com>
2022-12-09 16:14:16 +0200

    linux-imx8: Make the build pure
    - Add sha256 to make the build pure
    - Also change from builtins.fetchGit to fetchgit, this causes kernel
      source code to be fetched during build time, and not evaluation time.

--- pkgs/os-specific/linux/kernel/linux-imx8.nix
+++ pkgs/os-specific/linux/kernel/linux-imx8.nix
@@ -1,10 +1,9 @@
-{ lib, stdenv, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
+{ lib, stdenv, buildPackages, fetchgit, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
 
 with lib;
 
 buildLinux (args // rec {
   version = "5.15.32";
-  nxp_ref = "lf-5.15.y";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -26,8 +25,9 @@ buildLinux (args // rec {
     FB_EFI n
   '';
 
-  src = fetchGit {
+  src = fetchgit {
     url = "https://source.codeaurora.org/external/imx/linux-imx";
-    ref = nxp_ref;
+    rev = "fa6c3168595c02bd9d5366fcc28c9e7304947a3d"; # lf-5.15.y
+    sha256 = "sha256-Z24CpePoP28tBumTGc8ZUm/5WghuhtO2jvPOKtNaYGA=";
   };
 } // (args.argsOverride or { }))
